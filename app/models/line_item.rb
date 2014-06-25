80e2182c9ec81a62aaf08c7a9965aaca29826064
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-06-25 09:40:21 +0200

    Add decrement_quantity instance method.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,12 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
+
+  def decrement_quantity
+    if quantity > 1
+      update_attribute(:quantity, quantity - 1)
+    else
+      destroy
+    end
+  end
 end
