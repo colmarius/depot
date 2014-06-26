dfd2570cb4368582f9e56353a4ab0c7a7e017561
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-06-26 10:13:18 +0200

    Line item has one order. Order can have one or more line items.
    Note: when order is destroyed, associated line items are also destroyed.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
   PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
 
   validates :name, :address, :email, presence: true
